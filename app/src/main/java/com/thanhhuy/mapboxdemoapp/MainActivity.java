ac36190ff335baa4ccb0bfbbcaeaaddecc613172
Phan Thanh Huy <huy3999@gmail.com>
2019-09-28 09:41:38 +0700

    update

--- app/src/main/java/com/thanhhuy/mapboxdemoapp/MainActivity.java
+++ app/src/main/java/com/thanhhuy/mapboxdemoapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.thanhhuy.mapboxdemoapp;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.os.Bundle;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListPopupWindow;
+import android.text.TextUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 // classes needed to initialize map
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
+import com.mapbox.core.exceptions.ServicesException;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
@@ -24,13 +56,17 @@ import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 
 // classes to calculate a route
+import com.mapbox.mapboxsdk.style.sources.ImageSource;
 import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
 import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
 import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
@@ -39,6 +75,8 @@ import com.mapbox.api.directions.v5.models.DirectionsRoute;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
+
 import android.util.Log;
 
 // classes needed to launch navigation UI
@@ -56,11 +94,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private PermissionsManager permissionsManager;
     private LocationComponent locationComponent;
     // variables for calculating and drawing a route
+    private Point originPosition;
+    private Point destinationPosition;
     private DirectionsRoute currentRoute;
     private static final String TAG = "DirectionsActivity";
     private NavigationMapRoute navigationMapRoute;
     // variables needed to initialize navigation
     private Button button;
+
+    private Button chooseCityButton;
+    private EditText latEditText;
+    private EditText longEditText;
+    private TextView geocodeResultTextView;
+    private MapboxMap map;
+    private GeoFire geoFire;
+    private Map<String,Marker> markers;
+
+    private Handler handler;
+    private Runnable runnable;
+    private final String ID = "bike_location";
+    private final String URL_GET_DATA= "https://mapboxdemo-7716b.firebaseio.com/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,17 +124,239 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+//        mapView.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+//                map = mapboxMap;
+//
+//                mapboxMap.getStyle(new Style.OnStyleLoaded() {
+//                    @Override
+//                    public void onStyleLoaded(@NonNull Style style) {
+//
+//
+//                        try {
+//
+//                            GeoJsonSource source = new GeoJsonSource(ID, new URI(URL_GET_DATA));
+//
+//                            style.addSource(source);
+//
+//                        } catch (URISyntaxException exception) {
+//
+//
+//                        }
+//                        style.addSource(new ImageSource());
+//                    }
+//                });
+//
+//
+//            }
+//        });
+
     }
+
+
+//    private void getLocation(Style style){
+//        try {
+//            style.addSource(new GeoJsonSource(ID,new URL(URL_GET_DATA)));
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        //all layer
+//        SymbolLayer layer = new SymbolLayer(ID,ID);
+//        layer.setProperties(iconImage("mapbox_marker_icon_default"));
+//        style.addLayer(layer);
+//        //refresh data
+//        handler = new Handler();
+//        runnable = new refreshData(map,handler);
+//        handler.postDelayed(handler,2000);
+//
+//    }
+
+    private void initTextViews() {
+        latEditText = findViewById(R.id.geocode_latitude_editText);
+        longEditText = findViewById(R.id.geocode_longitude_editText);
+        geocodeResultTextView = findViewById(R.id.geocode_result_message);
+    }
+
+    private void initButtons() {
+        final Button mapCenterButton = findViewById(R.id.map_center_button);
+        Button startGeocodeButton = findViewById(R.id.start_geocode_button);
+        chooseCityButton = findViewById(R.id.choose_city_spinner_button);
+        startGeocodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Make sure the EditTexts aren't empty
+
+
+
+                if (TextUtils.isEmpty(latEditText.getText().toString())) {
+                    latEditText.setError(getString(R.string.fill_in_a_value));
+                } else if (TextUtils.isEmpty(longEditText.getText().toString())) {
+                    longEditText.setError(getString(R.string.fill_in_a_value));
+                } else {
+                    if (latCoordinateIsValid(Double.valueOf(latEditText.getText().toString()))
+                            && longCoordinateIsValid(Double.valueOf(longEditText.getText().toString()))) {
+// Make a geocoding search with the values inputted into the EditTexts
+                        makeGeocodeSearch(new LatLng(Double.valueOf(latEditText.getText().toString()),
+                                Double.valueOf(longEditText.getText().toString())));
+
+                    }
+
+                    else {
+                        Toast.makeText(MainActivity.this, R.string.make_valid_lat, Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            }
+        });
+
+        chooseCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCityListMenu();
+            }
+        });
+        mapCenterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Get the map's target
+                LatLng target = mapboxMap.getCameraPosition().target;
+
+// Fill the coordinate EditTexts with the target's coordinates
+                setCoordinateEditTexts(target);
+
+// Make a geocoding search with the target's coordinates
+                makeGeocodeSearch(target);
+
+
+            }
+        });
+    }
+
+    private boolean latCoordinateIsValid(double value) {
+        return value >= -90 && value <= 90;
+    }
+
+    private boolean longCoordinateIsValid(double value) {
+        return value >= -180 && value <= 180;
+    }
+
+    private void setCoordinateEditTexts(LatLng latLng) {
+        latEditText.setText(String.valueOf(latLng.getLatitude()));
+        longEditText.setText(String.valueOf(latLng.getLongitude()));
+    }
+
+    private void showCityListMenu() {
+        List<String> modes = new ArrayList<>();
+        modes.add("Vancouver");
+        modes.add("Helsinki");
+        modes.add("Lima");
+        modes.add("Osaka");
+        ArrayAdapter<String> profileAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, modes);
+        final ListPopupWindow listPopup = new ListPopupWindow(this);
+        listPopup.setAdapter(profileAdapter);
+        listPopup.setAnchorView(chooseCityButton);
+        listPopup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long longg) {
+                LatLng cityLatLng = new LatLng();
+                if (position == 0) {
+// Vancouver
+                    cityLatLng = new LatLng(49.2827, -123.1207);
+                    setCoordinateEditTexts(cityLatLng);
+                } else if (position == 1) {
+// Helsinki
+                    cityLatLng = new LatLng(60.1698, 24.938);
+                    setCoordinateEditTexts(cityLatLng);
+                } else if (position == 2) {
+// Lima
+                    cityLatLng = new LatLng(-12.0463, -77.0427);
+                    setCoordinateEditTexts(cityLatLng);
+                } else if (position == 3) {
+// Osaka
+                    cityLatLng = new LatLng(34.693, 135.5021);
+                    setCoordinateEditTexts(cityLatLng);
+                }
+                animateCameraToNewPosition(cityLatLng);
+                makeGeocodeSearch(cityLatLng);
+                listPopup.dismiss();
+            }
+        });
+        listPopup.show();
+    }
+
+    private void makeGeocodeSearch(final LatLng latLng) {
+        try {
+// Build a Mapbox geocoding request
+            MapboxGeocoding client = MapboxGeocoding.builder()
+                    .accessToken(getString(R.string.access_token))
+                    .query(Point.fromLngLat(latLng.getLongitude(), latLng.getLatitude()))
+                    .geocodingTypes(GeocodingCriteria.TYPE_PLACE)
+                    .mode(GeocodingCriteria.MODE_PLACES)
+                    .build();
+            client.enqueueCall(new Callback<GeocodingResponse>() {
+                @Override
+                public void onResponse(Call<GeocodingResponse> call,
+                                       Response<GeocodingResponse> response) {
+
+                    if (response.body() != null) {
+                        List<CarmenFeature> results = response.body().features();
+                        if (results.size() > 0) {
+
+// Get the first Feature from the successful geocoding response
+                            CarmenFeature feature = results.get(0);
+                            geocodeResultTextView.setText(feature.toString());
+                            animateCameraToNewPosition(latLng);
+
+
+
+                        } else {
+                            Toast.makeText(MainActivity.this, R.string.no_results,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                    Timber.e("Geocoding Failure: " + throwable.getMessage());
+                }
+            });
+        } catch (ServicesException servicesException) {
+            Timber.e("Error geocoding: " + servicesException.toString());
+            servicesException.printStackTrace();
+        }
+    }
+
+    private void animateCameraToNewPosition(LatLng latLng) {
+        mapboxMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(new CameraPosition.Builder()
+                        .target(latLng)
+                        .zoom(13)
+                        .build()), 1500);
+
+
+        //
+
+    }
+
+
+
+
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
+
         mapboxMap.setStyle(getString(R.string.navigation_guidance_day), new Style.OnStyleLoaded() {
             @Override
             public void onStyleLoaded(@NonNull Style style) {
+                //initTextViews();
+                //initButtons();
                 enableLocationComponent(style);
-
                 addDestinationIconSymbolLayer(style);
+
 
                 mapboxMap.addOnMapClickListener(MainActivity.this);
                 button = findViewById(R.id.startButton);
@@ -98,7 +374,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 });
             }
         });
+
+
+
     }
+
 
     private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
         loadedMapStyle.addImage("destination-icon-id",
@@ -112,6 +392,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 iconIgnorePlacement(true)
         );
         loadedMapStyle.addLayer(destinationSymbolLayer);
+
     }
 
     @SuppressWarnings( {"MissingPermission"})
@@ -126,6 +407,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (source != null) {
             source.setGeoJson(Feature.fromGeometry(destinationPoint));
         }
+        // click man hinh ra lat lng (hay vl)
+        //LatLng destinationLatLng = new LatLng();
+       // setCoordinateEditTexts(point);
 
         getRoute(originPoint, destinationPoint);
         button.setEnabled(true);
@@ -187,6 +471,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -206,6 +491,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             finish();
         }
     }
+
+
+
 
     @Override
     protected void onStart() {
@@ -248,4 +536,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+//    private class refreshData implements Runnable {
+//        private  MapboxMap map;
+//        private Handler handler;
+//        public refreshData(MapboxMap mapboxMap, Handler handler) {
+//            this.map = map;
+//            this.handler = handler;
+//        }
+//
+//        @Override
+//        public void run() {
+//
+//            ((GeoJsonSource)map.getSource())
+//        }
+//    }
 }
